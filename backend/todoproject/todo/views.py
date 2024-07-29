0fb0b953e765a7864e10db32733e7338e9a1b999
BrianBariyo <105715672+BRAGGZ9@users.noreply.github.com>
2024-07-29 19:20:37 +0300

    Complete backend api

--- backend/todoproject/todo/views.py
+++ backend/todoproject/todo/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+@api_view(['GET'])
+def api_home(request):
+    data = {
+        "message": "Welcome to the API!",
+    }
+    return Response(data)
